1e3ea8094e61583903e889c2db940374dd50a842
T-W-Smith <slicksmith14@gmail.com>
2022-11-07 17:37:06 -0500

    Add a function to loop the game 5 times and keep score

--- script.js
+++ script.js
@@ -13,21 +13,27 @@ function getComputerChoice () {
 
 function playARound (playerSelection, computerSelection) {
     if (computerSelection === "rock" && playerSelection === "scissors") {
+        computerScore++;
         return "You Lose! Rock beats Scissors"
     }
     else if (computerSelection === "paper" && playerSelection === "rock") {
+        computerScore++;
         return "You Lose! Paper beats Rock"
     }
     else if (computerSelection === "scissors" && playerSelection === "paper") {
+        computerScore++;
         return "You Lose! Scissors beats Paper"
     }
     else if (computerSelection === "rock" && playerSelection === "paper") {
+        playerScore++;
         return "You Win! Paper beats Rock"
     }
     else if (computerSelection === "paper" && playerSelection === "scissors") {
+        playerScore++;
         return "You Win! Scissors beats Paper"
     }
     else if (computerSelection === "scissors" && playerSelection === "rock") {
+        playerScore++;
         return "You win! Rock beats Scissors"
     }
     else {
@@ -35,6 +41,25 @@ function playARound (playerSelection, computerSelection) {
     }
 }
 
-const playerChoice = prompt("What do you choose?").toLowerCase();
-const computerChoice = getComputerChoice();
-console.log(playARound(playerChoice, computerChoice));
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const playerChoice = prompt("What do you choose?").toLowerCase();
+        const computerChoice = getComputerChoice();
+        playARound(playerChoice, computerChoice);
+        console.log("playerscore: " + playerScore + "   ComputerScore: " + computerScore);
+    }
+
+    if (playerScore > computerScore) {
+        return "You win!"
+    }
+    else if (computerScore > playerScore) {
+        return "You lose!"
+    }
+    else {
+        return "Its a Tie!"
+    }
+}
+
+let playerScore = 0;
+let computerScore = 0;
+console.log(game());
